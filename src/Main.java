d485f0ee3fc524eec6745a7a1b0718e4694ce546
Justin-Liao23-e <81964976+Justin-Liao23-e@users.noreply.github.com>
2023-04-29 22:09:42 -0700

    Day1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+// The main class will be performing arithmetic performance.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Scanner object 1 for user input.
+        Scanner sc = new Scanner(System.in);
+
+        // This stores the input by the
+        ArrayList<Integer> Ns = new ArrayList<>();
+
+        //
+        for (int i = 0; i < 3;i++ ){
+            int N1 = sc.nextInt();
+            Ns.add(N1);
+        }
+
+        System.out.println(Ns);
     }
 }
